71d98854bc14051158622ca0b834295b31d009a5
Elie Housseini <eliashousseini@gmail.com>
2020-09-14 12:00:18 +0300

    Visual layout: implementing Bootstrap, passing attributes, creating the layout

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -4,12 +4,20 @@ namespace App\Controller;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
-class ArticleController {
+class ArticleController extends AbstractController{
   /**
    * @Route("/")
+   * @Method({"GET"})
    */
   public function index(){
-    return new Response('<html><body>Hello</body></html>');
+    // return new Response('<html><body>Hello</body></html>');
+
+    $articles = ['Article 1', 'Article 2'];
+
+    // rending the page articles/index.html.twig
+    // passing the array arrticles under the name articles.
+    return $this->render('articles/index.html.twig', array('articles' => $articles));
   }
 }
